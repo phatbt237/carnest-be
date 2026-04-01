71757d59a434f9798add8be252e31a1c6a3513bc
Phat <phatbt237@gmail.com>
2026-04-01 23:18:56 +0700

    add more domain be

--- src/main/java/com/example/carnest/Config/SecurityConfig.java
+++ src/main/java/com/example/carnest/Config/SecurityConfig.java
@@ -99,7 +99,7 @@ public class SecurityConfig {
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://carnest-be-production.up.railway.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
