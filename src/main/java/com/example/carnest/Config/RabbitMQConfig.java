0dcf1cf2add7c12ff986896aaf43493e3ce5ae4f
Phat <phatbt237@gmail.com>
2026-05-05 16:35:11 +0700

    update code

--- src/main/java/com/example/carnest/Config/RabbitMQConfig.java
+++ src/main/java/com/example/carnest/Config/RabbitMQConfig.java
@@ -2,6 +2,7 @@ package com.example.carnest.Config;
 
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -117,5 +118,11 @@ public class RabbitMQConfig {
     }
     @Bean public Binding bindStatsUpdate() {
         return BindingBuilder.bind(statsUpdateQueue()).to(orderExchange()).with(RK_STATS_UPDATE);
+    }
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin admin = new RabbitAdmin(connectionFactory);
+        admin.setAutoStartup(true);
+        return admin;
     }
 }
